00794cf0a17f662c43adc0d40d812459ee269c40
Tiago Barreto <tiagotavaresbarreto@gmail.com>
2026-05-07 22:41:26 +0000

    add make_subplots import

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 import plotly.express as px
+import plotly.graph_objects as go
 
 from prophet import Prophet
 from plotly.subplots import make_subplots
